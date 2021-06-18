a9ec9d5513e10a3375347e96be293957573a954d
s3b4vc <84742004+s3b4vc@users.noreply.github.com>
2021-06-17 23:40:23 -0400

    Add files via upload

---
+++ Sueldo/rrhh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sueldo
+{
+    class rrhh
+    {
+
+        public double Calculo_Nomina(int sueldo)
+        {
+            double gratificacion = sueldo * 0.25;
+            double total_imponible = sueldo + gratificacion;
+            double afp = total_imponible * 0.11;
+            double fonasa = total_imponible * 0.7;
+            double cesantia = total_imponible * 0.06;
+            double total_final = total_imponible - afp - fonasa - cesantia;
+            return total_final;
+        }
+    }
+
+}
